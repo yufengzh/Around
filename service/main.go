3307513a67d93c41782b43307569468f804e6d28
Yufeng Zhang <zyf991108@gmail.com>
2018-07-19 22:35:41 -0700

    added elastic search post and query

--- service/main.go
+++ service/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"log"
 	"strconv"
+	elastic "gopkg.in/olivere/elastic.v3"
+	"reflect"
+	"github.com/pborman/uuid"
 )
 
 type Location struct {
@@ -21,11 +24,41 @@ type Post struct {
 }
 
 const (
+	INDEX = "around"
+	TYPE = "post"
 	DISTANCE = "200km"
+	ES_URL = "http://35.224.10.119:9200"
 )
 
 
 func main() {
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+	
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{																						"mappings":{																						"post":{
+					"properties":{																						"location":{																						"type":"geo_point"
+					          }
+					}
+				     }																					}
+		}`
+	_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
+
 	fmt.Println("started-service")
 	http.HandleFunc("/post", handlerPost)
 	http.HandleFunc("/search", handlerSearch)
@@ -51,6 +84,24 @@ func handlerPost(w http.ResponseWriter, r *http.Request) { // in Go we pass by v
 	}
 
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+
+	id := uuid.New()
+	//Save to ES
+	saveToES(&p, id)
+}
+
+func saveToES(p *Post, id string) {
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL),
+			elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = es_client.Index().Index(INDEX).Type(TYPE).Id(id).BodyJson(p).Refresh(true).Do()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Post is saved to index :%s\n", p.Message)
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -65,23 +116,43 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		ran = val + "km"
 	}
 	
-	fmt.Println("range is", ran);	
-	
-	// Return a fake post
-	p := &Post {
-		User: "1111",
-		Message: "一生必去的100个地方",
-		Location: Location {
-			Lat: lat,
-			Lon : lon,
-		},
-	}
+	fmt.Printf("Search received. %f % f %s\n", lat, lon, ran)
 
-	js, err := json.Marshal(p)
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false)) // sniff : elastic's library, there is a callback to do logging.
 	if err != nil {
 		panic(err)
-	}	
+	}
+
+	q := elastic.NewGeoDistanceQuery("location") // query name
+	q = q.Distance(ran).Lat(lat).Lon(lon) // return itself each time when you call
+
+	searchResult, err := client.Search().
+			Index(INDEX).
+			Query(q).
+			Pretty(true).
+			Do()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	fmt.Printf("Found a total of %d posts \n", searchResult.TotalHits())
+
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // equivalent to Java instanceOf
+		p := item.(Post) // p = (Post) item //casting
+		fmt.Printf("post by %s: %s at lat %v and long %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		ps = append(ps, p) // we put all the search results in the slide ps
+	}
+
+	js, err := json.Marshal(ps)
+	if err != nil {
+		panic(err)
+	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
+
 }
