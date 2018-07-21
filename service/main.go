dbf044ca528e37500357aa742db24f4fc6d729e9
Yufeng Zhang <zyf991108@gmail.com>
2018-07-20 21:45:36 -0700

    add Google Cloud Storage to store images and videos

--- service/main.go
+++ service/main.go
@@ -10,6 +10,9 @@ import (
 	"reflect"
 	"strings"
 	"github.com/pborman/uuid"
+	"context"
+	"cloud.google.com/go/storage"
+	"io"
 )
 
 type Location struct {
@@ -22,13 +25,16 @@ type Post struct {
 	User string `json:"user"`
 	Message string `json:"message"`
 	Location Location `json:"location"` 
+	Url string `json:"url"`
 }
+
 
 const (
 	INDEX = "around"
 	TYPE = "post"
 	DISTANCE = "200km"
-	ES_URL = "http://35.225.221.66:9200"
+	ES_URL = "http://35.202.220.181:9200"
+	BUCKET_NAME = "around-prod"
 )
 
 
@@ -77,18 +83,90 @@ func main() {
 
 
 func handlerPost(w http.ResponseWriter, r *http.Request) { // in Go we pass by value, so we can use pointer to avoid deep copy
-	fmt.Println("Received one post request.")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Header", "Content-Type,Authorization")
+	
+	r.ParseMultipartForm(32 << 20)
+
+	//Parse form data
+	fmt.Printf("Received one post request%s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+
+	p := &Post{
+		User : "1111",
+		Message: r.FormValue("message"),
+		Location: Location {
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	id := uuid.New()
+	
+	file,_,err := r.FormFile("image")
+	// jubing, fileHeader, err
+	if err != nil {
+		http.Error(w, "image is not setup", http.StatusInternalServerError)
+		fmt.Printf("image is not setup %v\n", err) // we can use %v to output any type of data
+		panic(err)
+	}
+	
+	defer file.Close()
+
+	ctx := context.Background()
+
+	_, attrs, err := saveToGCS(ctx,file, BUCKET_NAME, id)
+	
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
 		panic(err)
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	p.Url = attrs.MediaLink
 
-	id := uuid.New()
 	//Save to ES
-	saveToES(&p, id)
+	saveToES(p, id) // p is now a pointer so take the % off
+}
+
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	// as long as we implement io.Reader interface, we can use io.Copy
+	client, err := storage.NewClient(ctx)
+	
+	if err != nil {
+		return nil, nil, err
+	}
+
+	defer client.Close()
+
+	bucket := client.Bucket(BUCKET_NAME)
+	
+	// Next check if the bucket exists
+	if _, err := bucket.Attrs(ctx); err != nil {
+		return nil, nil, err 
+	}
+
+	obj := bucket.Object(name)
+	wc := obj.NewWriter(ctx) // writer client
+
+	if _, err := io.Copy(wc, r); err != nil {
+		return nil, nil, err
+	}
+
+	if err := wc.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+
+	return obj, attrs, err
 }
 
 func saveToES(p *Post, id string) {
